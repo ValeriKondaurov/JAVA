a550456d5478acb2cd8b1b4fb2b9eb3ac7f2d411
VKondaurov <kondaurov_vv@mail.ru>
2018-03-16 22:28:30 +0300

    Java3HomeWork6 update
    Update code. Adjusted the code for a parameterized test run

--- j3HW6/MyArrayTest.java
+++ j3HW6/MyArrayTest.java
@@ -1,29 +1,47 @@
+/**
+ JUnit test for check class MyArrays
+
+ * @author Valeriy Kondaurov
+ * @version dated MAR 16, 2018
+ * @link https://github.com/ValeriKondaurov/Java3
+ */
+
 import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-
 import java.util.*;
 
 
-
+@RunWith(Parameterized. class)
 public class MyArrayTest {
+    @Parameterized.Parameters
+    public static Collection data() {
+        return Arrays. asList(new int [][][]{
+                {{1,2,3,4,5,6},   {5,6}},
+                {{1,4,3,2,5,6},   {3,2,5,6}},
+                {{1,2,3,5,6},   {}},
+        });
+    }
+    private int[] in;
+    private int[] out;
+
+    public MyArrayTest(int [] in, int [] out) {
+        this.in = in;
+        this.out = out;
+    }
+
+
     MyArrays myarrays;
     @Before
     public void init() {
         myarrays = new MyArrays();
     }
     @Test
-    public void massTestPrint() {
-        Assert.assertTrue(myarrays.print(new int[] {1,2,3,4,5}));
-    }
-    @Test
     public void massTestArrayAfterFour() {
-        Assert.assertArrayEquals(new int[] {5,6}, myarrays.ArrayAfterFour(new int[] {1,2,3,4,5,6}));
-
+        Assert.assertArrayEquals(out, myarrays.ArrayAfterFour(in));
     }
     @Test
     public void massTestArrayOnlyOneOrFour() {
-        Assert.assertFalse(myarrays.ArrayOnlyOneOrFour(new int[] {1,2,3,4,5,6}));
-
+        Assert.assertFalse(myarrays.ArrayOnlyOneOrFour(in));
     }
 }
--- j3HW6/MyArrays.java
+++ j3HW6/MyArrays.java
@@ -1,18 +1,35 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+/**
+ 1. A method to which a non-empty one-dimensional integer array
+ is passed as an argument must return a new array that is obtained
+ by pulling elements from the original array coming after the last four.
+ The input array must contain at least one four,
+ otherwise RuntimeException must be thrown in the method.
+
+ 2. A method that verifies that an array consists of only numbers 1 and 4.
+ If the array does not have at least one 4 or 1, the method returns false;*
+
+ * @author Valeriy Kondaurov
+ * @version dated MAR 16, 2018
+ * @link https://github.com/ValeriKondaurov/Java3
+ */
+
 import java.util.Arrays;
 
 public class MyArrays {
     public int[] ArrayAfterFour(int[] a) {
         int i = a.length;
-       do {
-            i--;
-        } while (!(a[i]==4));
-        i++;
-        int[] b =  new int[a.length-i];
-        for (int j = 0;  j<b.length; j++) b[j] = a[i+j];
-
-        return b;
+        try {
+            do {
+                i--;
+            } while (!(a[i] == 4));
+        } catch (RuntimeException e){
+            e.fillInStackTrace();
+        } finally {
+            i++;
+            int[] b = new int[a.length - i];
+            for (int j = 0; j < b.length; j++) b[j] = a[i + j];
+            return b;
+        }
     }
 
     public boolean ArrayOnlyOneOrFour(int[] a) {
@@ -35,15 +52,10 @@ public class MyArrays {
 
     }
 
-    public  boolean print (int[] a) {
-        System.out.println(Arrays.toString(a));
-        return true;
-    }
-
 
     public static void main(String[] args) {
         MyArrays m = new MyArrays();
-        int[] b = m.ArrayAfterFour(new int[]{1, 2, 3, 4, 5, 6});
+        int[] b = m.ArrayAfterFour(new int[]{1, 2, 3, 5, 6});
         System.out.println(Arrays.toString(b));
         System.out.println(m.ArrayOnlyOneOrFour(new int[] {1, 2, 3, 4, 5, 6}));
     }
